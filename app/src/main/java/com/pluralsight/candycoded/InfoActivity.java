04691656d14f45b0b59e1222dce40da2a7140ced
Solomon-Okwir <66220262+Solomon-Okwir@users.noreply.github.com>
2020-09-25 17:17:19 +0300

    Module4

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -38,7 +38,14 @@ public class InfoActivity extends AppCompatActivity {
 
     // ***
 
-    // ***
+    // ***method createPhoneIntent()
     // TODO - Task 3 - Launch the Phone Activity
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL,uri);
+        if(phoneIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(phoneIntent);
+        }
+    }
     // ***
 }
